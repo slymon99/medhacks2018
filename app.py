8ddbf57e3fc84b2488dbf1de83c9d43d7e0ebffe
Simon Clark <simonclark236@gmail.com>
2018-09-08 09:20:49 -0400

    refactored fruit and vegetable calls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from db import getConnection, dbExecute
 app = Flask(__name__)
 
@@ -11,17 +11,31 @@ def hello_world():
 
 @app.route('/api/v1.0/fruit')
 def getFruit():
-	c = dbExecute(getConnection(), 'SELECT * FROM Fruit')
-	fruits = c.fetchall()
-	fruits_parsed = {"results": {x[0]: x[1] for x in fruits}}
-	return jsonify(fruits_parsed)
+	return getItems("Fruit")
 
 @app.route('/api/v1.0/veg')
-def getFruit():
-	c = dbExecute(getConnection(), 'SELECT * FROM Veg')
-	veg = c.fetchall()
-	veg_parsed = {"results": {x[0]: x[1] for x in veg}}
-	return jsonify(veg_parsed)
+def getVeg():
+	return getItems("Vegetable")
+
+def getItems(category):
+	c = dbExecute(getConnection(), 'SELECT * FROM Items WHERE Type = ?', (category,))
+	items = c.fetchall()
+	parsed = {"results": {x[1]: x[2] for x in items}}
+	return jsonify(parsed)
+
+@app.route('/api/v1.0/addOrder', methods=["POST"])
+def addOrder():
+	if not request.json:
+		abort(400)
+	
+	#get current max id
+	c = dbExecute(getConnection(), 'SELECT max(ID) from Orders')
+	max_id = c.fetchone()[0]
+
+	conn = getConnection()
+	dbExecute(conn, "INSERT INTO Orders VALUES(?, ?, ?, ?)", (max_id + 1, request.json['contents'], request.json['locationID'], request.json['customerID']))
+	conn.commit()
+	return jsonify({"id":max_id + 1})
 
 
 if __name__ == "__main__":
