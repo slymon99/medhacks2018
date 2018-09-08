b0972450e255354f0ce1dde80408d3fb8f9d02e3
stanford <stanfordlin@outlook.com>
2018-09-08 10:34:06 -0400

    updated flask

--- foodpriority/server/server.py
+++ foodpriority/server/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request
 from db import getConnection, dbExecute
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+import sqlite3
+import json
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/api/v1.0/fruit')
 def getFruit():
@@ -37,6 +39,12 @@ def addOrder():
 	conn.commit()
 	return jsonify({"id":max_id + 1})
 
+@app.route('/api/v1.0/destinations')
+def mergeOrders():
+
+	#get all current orders
+	c = dbExecute(getConnection(), 'SELECT LocationID, GROUP_CONCAT(Contents) FROM Orders GROUP BY LocationID')
+	return jsonify({"results": c.fetchall()})
 
 
 if __name__ == "__main__":
