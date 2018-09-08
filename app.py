42f398eb5c21b3747ce1af5dbbda6642564cee17
Simon Clark <simonclark236@gmail.com>
2018-09-08 08:16:54 -0400

    added db interaction and /fruit endpoint

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from db import getConnection, dbExecute
 app = Flask(__name__)
 
-import sqlite
+import sqlite3
 import json
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
+@app.route('/api/v1.0/fruit')
+def getFruit():
+	c = dbExecute(getConnection(), 'SELECT * FROM Fruit')
+	res = c.fetchall()
+	return jsonify({"results": res})
 
 if __name__ == "__main__":
 	app.run(debug = True)
