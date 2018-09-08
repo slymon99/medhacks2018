49bcc3443ffef5cde92ba5bd5d3c85339bf9ac36
stanford <stanfordlin@outlook.com>
2018-09-08 16:51:55 -0400

    Added map and click trigger

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
-import MyMapComponent from './ui/MyMapComponent';
+import MyFancyComponent from './ui/MyMapComponent';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class App extends React.Component {
+
+
+      
+
     render(props) {
         return (
             <div>My Flask React App!
               
-              <MyMapComponent
+              <MyFancyComponent
                 isMarkerShown
                 googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
-                />
+                                />
 
 
                 <Button variant="contained" color="primary">
--- src/ui/MyMapComponent.js
+++ src/ui/MyMapComponent.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React from "react"
+import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: 39.286897, lng: -76.641673 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 39.286897, lng: -76.641673 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 39.286897, lng: -76.641673 }} onClick={props.onMarkerClick} />}
   </GoogleMap>
-))
+);
 
-export default MyMapComponent;
+class MyFancyComponent extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+    console.log("hiiiii")
+  }
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
+
+export default MyFancyComponent;
