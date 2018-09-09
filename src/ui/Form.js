be91152394be33c06a9e9f9a5b20fe68d85e6434
stanford <stanfordlin@outlook.com>
2018-09-09 03:45:48 -0400

    added new values

--- src/ui/Form.js
+++ src/ui/Form.js
@@ -23,7 +23,7 @@ class Form extends React.Component {
 
     state = {
         appleQty: '',
-        orangeQty: '',
+        orangeQty: '0',
     };
 
 
@@ -55,17 +55,25 @@ class Form extends React.Component {
         const { classes } = this.props;
 
         let id = 0;
-        function createData(name, calories, fat, carbs, protein) {
+        function createData(category, name, price, servings, qty) {
           id += 1;
-          return { id, name, calories, fat, carbs, protein };
+          return { id, category, name, price, servings, qty };
         }
         
         const rows = [
-          createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-          createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-          createData('Eclair', 262, 16.0, 24, 6.0),
-          createData('Cupcake', 305, 3.7, 67, 4.3),
-          createData('Gingerbread', 356, 16.0, 49, 3.9),
+            createData('Fruit', 'Apples', 1.18, 3, 0),
+            createData('Fruit', 'Oranges', .83, 2, 0),
+            createData('Fruit', 'Bananas', .45, 3, 0),
+            createData('Fruit', 'Blackberries', 5.18, 3, 0),
+            createData('Fruit', 'Blueberries', 3.91, 3, 0),
+            createData('Vegetables', 'Broccoli', 1.84,3, 0),
+            createData('Vegetables', 'Celery', 2.25, 3,0),
+            createData('Vegetables', 'Carrots', 1.40, 3, 0),
+            createData('Vegetables', 'Lettuce', 1.95, 3,0),
+            createData('Vegetables', 'Spinach', 3.92, 3,0),
+            createData('Fruit', 'Grapefruit', .66,3, 0),
+            createData('Fruit', 'Mangoes', 1.00, 3,0),
+            createData('Fruit', 'Honeydew', .55, 3,0),
         ];
 
 
@@ -99,16 +107,15 @@ class Form extends React.Component {
                     Submit
                 </Button>
 
-
                         <Paper >
                             <Table >
                                 <TableHead>
                                     <TableRow>
-                                        <TableCell>Dessert (100g serving)</TableCell>
-                                        <TableCell numeric>Calories</TableCell>
-                                        <TableCell numeric>Fat (g)</TableCell>
-                                        <TableCell numeric>Carbs (g)</TableCell>
-                                        <TableCell numeric>Protein (g)</TableCell>
+                                        <TableCell>Category</TableCell>
+                                        <TableCell numeric>Name</TableCell>
+                                        <TableCell numeric>Price ($)</TableCell>
+                                        <TableCell numeric>Servings </TableCell>
+                                        <TableCell numeric>Qty</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
@@ -116,12 +123,14 @@ class Form extends React.Component {
                                         return (
                                             <TableRow key={row.id}>
                                                 <TableCell component="th" scope="row">
+                                                    {row.category}
+                                                </TableCell>
+                                                <TableCell component="th" scope="row">
                                                     {row.name}
                                                 </TableCell>
-                                                <TableCell numeric>{row.calories}</TableCell>
-                                                <TableCell numeric>{row.fat}</TableCell>
-                                                <TableCell numeric>{row.carbs}</TableCell>
-                                                <TableCell numeric><TextField defaultValue={row.protein}></TextField></TableCell>
+                                                <TableCell numeric>{row.price}</TableCell>
+                                                <TableCell numeric>{row.servings}</TableCell>
+                                                <TableCell numeric><TextField defaultValue={row.qty} onChange={this.handleChange(row.qty)}></TextField></TableCell>
                                             </TableRow>
                                         );
                                     })}
